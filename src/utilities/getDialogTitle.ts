ea9849d50f042df45a44eca960188f8029bf4cd2
Boris Topolnjak <boris.topolnjak@gmail.com>
2023-10-30 14:06:04 +0100

    removed unecessary functions

--- src/utilities/getDialogTitle.ts
+++
@@ -1,5 +0,0 @@
-import { DIALOG_TITLE_MAP } from "../constants";
-
-export default function getDialogTitle(name: string) {
-  return DIALOG_TITLE_MAP[name];
-}
--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -3,6 +3,4 @@ export { default as getMovies } from "./getMovies";
 export { default as getMovieById } from "./getMovieById";
 export { default as getReleaseYearFromDate } from "./getReleaseYearFromDate";
 export { default as getCurrentDate } from "./getCurrentDate";
-export { default as getDialogType } from "./getDialogType";
-export { default as getDialogTitle } from "./getDialogTitle";
 export { handleMovie, deleteMovie } from "./handleMovie";
